eaaa772fe57efc1ec552b4a26ef2968f4f1da4c0
ajitzz <93880885+ajitzz@users.noreply.github.com>
2026-01-04 01:43:48 +0530

    Update numbers.ts

--- lib/data/numbers.ts
+++ lib/data/numbers.ts
@@ -3,6 +3,6 @@ export const numbers = {
   coreTeams: 24,
   earnings: 8500000,
   locationsTitle: "Operational Cities",
-  locations: ["Bangalore", "Hyderabad", "Chennai", "Mumbai"],
+  locations: ["Madiwala", "Electronic City", "Marathahalli",
   image: "https://i.ibb.co/Z1gCBcbt/c837a6-3a4f054db4424af1b034f96ddd7b691a-mv2.jpg",
 };
