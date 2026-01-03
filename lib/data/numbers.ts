2129edd2c4d8f4da5f8b4cf83a5b70cbbb49487f
ajitzz <93880885+ajitzz@users.noreply.github.com>
2026-01-04 01:41:15 +0530

    Update numbers.ts

--- lib/data/numbers.ts
+++ lib/data/numbers.ts
@@ -4,5 +4,5 @@ export const numbers = {
   earnings: 8500000,
   locationsTitle: "Operational Cities",
   locations: ["Bangalore", "Hyderabad", "Chennai", "Mumbai"],
-  image: "https://i.ibb.co/dKRhhPv/Adobe-Express-file.png",
+  image: "https://i.ibb.co/Z1gCBcbt/c837a6-3a4f054db4424af1b034f96ddd7b691a-mv2.jpg",
 };
