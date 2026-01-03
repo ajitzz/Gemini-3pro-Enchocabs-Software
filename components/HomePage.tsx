dbbad5577dcdaff760aec5a93a0dae113cfa32c2
ajitzz <93880885+ajitzz@users.noreply.github.com>
2026-01-04 00:41:25 +0530

    Update HomePage.tsx

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -11,21 +11,18 @@ import Footer from './HomeComponents/footer/Footer';
 import PartTime from './HomeComponents/PartTime/PartTime';
 export default function HomePage() {
   return (
-  <main className="min-h-screen bg-neutral-950 px-4 py-8 sm:px-6 lg:px-10">
-      <div className="mx-auto flex max-w-screen-2xl flex-col overflow-hidden rounded-3xl border border-neutral-200 bg-white shadow-2xl">
-        <div className="flex flex-col divide-y divide-neutral-200">
-          <Hero />
-          <Vision />
-          <Services />
-          <Rental />
-          <PartTime />
-          <Salary />
-          <Parallax />
-          <Numbers />
-          <Testimonials items={[]} autoplay interval={3500} />
-          <Footer />
-        </div>
-      </div>
-    </main>
+  <>
+  
+      <Hero />
+      <Vision />
+      <Services />
+      <Rental />
+      <PartTime/>
+      <Salary />
+      <Parallax />
+      <Numbers />
+     <Testimonials items={[]} autoplay interval={3500} />
+      <Footer />
+    </>
   );
 }
