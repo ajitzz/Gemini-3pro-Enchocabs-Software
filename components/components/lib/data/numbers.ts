5b30236e00999a4ce7886bf4d77dab0901c4dd35
ajitzz <93880885+ajitzz@users.noreply.github.com>
2026-01-03 22:03:18 +0530

    Delete components/components/lib directory

--- components/components/lib/data/numbers.ts
+++
@@ -1,9 +0,0 @@
-export const numbers = {
-  drivers: 40,
-  coreTeams: 5,
-  earnings: 75000,
-  locationsTitle: 'Locations (Bangalore)',
-  locations: ['Madiwala', 'Bagaluru', 'Marthahalli', 'Electronic City'],
-  image:
-    'https://i.ibb.co/Z1gCBcbt/c837a6-3a4f054db4424af1b034f96ddd7b691a-mv2.jpg',
-};
