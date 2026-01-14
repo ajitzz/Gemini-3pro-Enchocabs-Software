666135df9fc9547f8f0c897edc48000a5b799de8
ajitzz <93880885+ajitzz@users.noreply.github.com>
2026-01-14 21:05:27 +0530

    Update numbers.ts

--- lib/data/numbers.ts
+++ lib/data/numbers.ts
@@ -1,7 +1,7 @@
 export const numbers = {
-  drivers: 20,
+  drivers: 30,
   coreTeams: 3,
-  earnings: 7500000,
+  earnings: 75,000,
   locationsTitle: "Operational Cities Bangalore",
   locations: ["Madiwala", "Electronic City", "Marathahalli",],
   image: "https://i.ibb.co/Z1gCBcbt/c837a6-3a4f054db4424af1b034f96ddd7b691a-mv2.jpg",
